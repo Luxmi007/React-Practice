f39c53e237734e3878e99f507c50833ca78a2e6b
Luxmi <luxmi5829@gmail.com>
2024-04-30 23:46:17 +0530

    created react app

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {}, []);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Button />
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+function Button() {
+  const [val, setVal] = useState(0);
+  return (
+    <button onClick={() => setVal((value) => value + 1)}>New button count is {val}</button>
+  );
+}
+
+export default App;
